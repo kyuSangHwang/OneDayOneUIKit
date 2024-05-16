f8c5da948a3cc10f253112bcced2c806cd7b6c41
DopeKS <kyusang.dev@gmail.com>
2024-05-17 01:55:13 +0900

    인스타 화면 구성 배치하기 1 : 프로필 영역

--- ODO-UIKit/ODO-UIKit/ViewController.swift
+++ ODO-UIKit/ODO-UIKit/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let circleView: UIView = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // circleView를 view의 하위 뷰로 추가
+        view.addSubview(circleView)
+        
+        // circleView의 translatesAutoresizingMaskIntoConstraints 속성 설정
+        circleView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let safeArea = view.safeAreaLayoutGuide
+        
+        // 제약 조건 설정 및 활성화
+        NSLayoutConstraint.activate([
+            circleView.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 15),
+            circleView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 25),
+            circleView.widthAnchor.constraint(equalToConstant: 45),
+            circleView.heightAnchor.constraint(equalTo: circleView.widthAnchor)
+        ])
+        
+        // circleView의 배경색 설정 (원 모양 확인을 위해)
+        circleView.backgroundColor = .red
+        circleView.layer.cornerRadius = 45/2
     }
-
-
 }
+
 
