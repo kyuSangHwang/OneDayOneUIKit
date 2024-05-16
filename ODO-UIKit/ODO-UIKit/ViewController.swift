b011f5b1e96bb2f8390899d91e186460541617f6
DopeKS <kyusang.dev@gmail.com>
2024-05-17 01:55:35 +0900

    인스타 화면 구성 배치하기 2 : 닉네임 영역

--- ODO-UIKit/ODO-UIKit/ViewController.swift
+++ ODO-UIKit/ODO-UIKit/ViewController.swift
@@ -10,15 +10,18 @@ import UIKit
 class ViewController: UIViewController {
     
     let circleView: UIView = UIView()
+    let nickNameView: UIView = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // circleView를 view의 하위 뷰로 추가
         view.addSubview(circleView)
+        view.addSubview(nickNameView)
         
         // circleView의 translatesAutoresizingMaskIntoConstraints 속성 설정
         circleView.translatesAutoresizingMaskIntoConstraints = false
+        nickNameView.translatesAutoresizingMaskIntoConstraints = false
         
         let safeArea = view.safeAreaLayoutGuide
         
@@ -27,12 +30,20 @@ class ViewController: UIViewController {
             circleView.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 15),
             circleView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 25),
             circleView.widthAnchor.constraint(equalToConstant: 45),
-            circleView.heightAnchor.constraint(equalTo: circleView.widthAnchor)
+            circleView.heightAnchor.constraint(equalTo: circleView.widthAnchor),
+            
+            nickNameView.centerYAnchor.constraint(equalTo: circleView.centerYAnchor),
+            nickNameView.leadingAnchor.constraint(equalTo: circleView.trailingAnchor, constant: 15),
+            nickNameView.widthAnchor.constraint(equalToConstant: 200),
+            nickNameView.heightAnchor.constraint(equalToConstant: 40)
+            
         ])
         
         // circleView의 배경색 설정 (원 모양 확인을 위해)
         circleView.backgroundColor = .red
         circleView.layer.cornerRadius = 45/2
+        
+        nickNameView.backgroundColor = .red
     }
 }
 
